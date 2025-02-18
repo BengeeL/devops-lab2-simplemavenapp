4792961fc148c0edd20c36e6de1edc54f07894de
Benjamin Lefebvre <blefebv1@my.centennialcollege.ca>
2025-02-18 15:05:18 -0500

    Initial commit: Maven web app

---
+++ src/test/java/com/example/devops_lab_2/DevOpsLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_lab_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
